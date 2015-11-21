35e53f4ba44b0b1c49b8241555778b2432c02d9e
Logan Garrett <logan@Logans-MacBook-Pro.local>
2015-11-21 16:17:31 -0500

    bash

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-docker rm -f rancher-proxy
+docker rm -f rancher-proxy > /dev/null 2>&1
 docker run \
   --name="rancher-proxy" \
   --publish=443:443 \
   --publish=80:80 \
   --restart="always" \
-  --volume nginx.conf:/etc/nginx/nginx.conf:ro
-  --volume server.crt:/etc/ssl/server.crt
-  --volume server.key:/etc/ssl/server.key
+  --volume nginx.conf:/etc/nginx/nginx.conf:ro \
+  --volume server.crt:/etc/ssl/server.crt \
+  --volume server.key:/etc/ssl/server.key \
   --detach=true \
   nginx
