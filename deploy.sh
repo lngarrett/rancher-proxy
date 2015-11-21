a181d2ef1f645ffcd22465f2959ce3b8b028cc45
Logan Garrett <logan@Logans-MacBook-Pro.local>
2015-11-21 16:14:45 -0500

    proxy

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker rm -f rancher-proxy
+docker run \
+  --name="rancher-proxy" \
+  --publish=443:443 \
+  --publish=80:80 \
+  --restart="always" \
+  --volume nginx.conf:/etc/nginx/nginx.conf:ro
+  --volume server.crt:/etc/ssl/server.crt
+  --volume server.key:/etc/ssl/server.key
+  --detach=true \
+  nginx
